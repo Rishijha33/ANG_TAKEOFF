ae341acc0f4129d39d388467e7285c6dfa88a2dd
Rishijha33 <Rishijha3033@gmail.com>
2021-08-10 11:23:31 +0530

    second commir -rio

--- ANGTAKEOFF2/src/app/app.module.ts
+++ ANGTAKEOFF2/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UserLoginComponent } from './user/user-login/user-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
+    UserComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
